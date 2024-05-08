ae1f8c73ed00e8aedf882708ac6ab258f8240280
YunChen31 <luosammy11@gmail.com>
2024-05-08 15:58:19 +0800

    完成羅允晨的部分

--- FirstClass12Team.java
+++ FirstClass12Team.java
@@ -3,5 +3,6 @@ public class FirstClass12Team{
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157120\"\n姓名：\"鍾佩妤\""); 
         System.out.println("\"學號：\"01157104\"\n姓名：\"楊晴雯\""); 
+        System.out.println("\"學號：\"01157140\"\n姓名：\"羅允晨\""); 
     }
 }
