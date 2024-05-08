6ac2e4e3d7576b0e3d8eaff5bb4e3a140cb9b923
yu1547 <ppeennnnyy1547@gmail.com>
2024-05-08 15:32:05 +0800

    完成程式骨幹和鍾佩妤的部分

---
+++ FirstClass12Team.java
@@ -0,0 +1,6 @@
+public class FirstClass4Team{
+    publicstaticvoidmain(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01157120\"\n姓名：\"鍾佩妤\""); 
+    }
+}
